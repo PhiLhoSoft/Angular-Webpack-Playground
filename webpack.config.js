5a8cf81d63b1f823848c027eede7597f3ad9ccfe
PhiLho <PhiLho@GMX.net>
2017-01-31 12:02:42 +0100

    Move test config out to simplify config (less conditionals)
    And fix prod's image handling...

--- webpack.config.js
+++ webpack.config.js
@@ -53,15 +53,64 @@ if (!stats)
 // Collect all dependencies declared in package.json
 var dependencies = require('./package.json').dependencies;
 
-module.exports = makeWebpackConfig();
+/**
+ * Config
+ * Reference: http://webpack.github.io/docs/configuration.html
+ * This is the object where all configuration gets set
+ */
+var config;
+if (isTest)
+{
+	// Configuration is much simpler in test (no CSS, etc.)
+	config =
+	{
+		devtool: 'inline-source-map',
+
+		module:
+		{
+			preLoaders: [],
+			loaders:
+			[
+				{
+					// STYLUS LOADER
+					test: /\.styl$/,
+					loader: 'null'
+				},
+				{
+					// CSS LOADER
+					// For CSS files from modules.
+					test: /\.css$/,
+					loader: 'null'
+				},
+			]
+		},
+
+		plugins: [],
+	};
+	if (!isDebugTest)
+	{
+		// ISTANBUL LOADER
+		// Reference: https://github.com/deepsweet/istanbul-instrumenter-loader
+		// Instrument JS files with Istanbul for subsequent code coverage reporting.
+		// Skips node_modules and files that end with .test.js.
+		config.module.preLoaders.push(
+			{
+				test: /\.js$/,
+				include: nodePath.resolve(srcScriptPath),
+				exclude: /\.test\.js$/,
+				loader: 'istanbul-instrumenter'
+			}
+		);
+	}
+}
+else
+{
+	config = makeWebpackConfig();
+}
+config = addCommonPlugins(addCommonLoaders(addResolve(config)));
 
 function makeWebpackConfig()
 {
-	/**
-	 * Config
-	 * Reference: http://webpack.github.io/docs/configuration.html
-	 * This is the object where all configuration gets set
-	 */
 	var config = {};
 
 	/**
@@ -70,7 +119,7 @@ function makeWebpackConfig()
 	 * Should be an empty object if it's generating a test build
 	 * Karma will set this when it's a test build
 	 */
-	config.entry = isTest ? {} :
+	config.entry =
 	{
 		app: srcScriptPath + 'app.js',
 		// Defines the modules that go to the 'vendor' bundle.
@@ -83,7 +132,7 @@ function makeWebpackConfig()
 	 * Should be an empty object if it's generating a test build.
 	 * Karma will handle setting it up for you when it's a test build.
 	 */
-	config.output = isTest ? {} :
+	config.output =
 	{
 		// Absolute output directory
 		path: nodePath.resolve('public/'),
@@ -103,27 +152,6 @@ function makeWebpackConfig()
 	};
 
 	/**
-	 * Resolve
-	 * Reference: http://webpack.github.io/docs/configuration.html#resolve
-	 * Alias some paths, to avoid using require('../../foo') depending on depth in hierarchy...
-	 */
-	config.resolve =
-	{
-		alias:
-		{
-			// awp for angular-webpack-playground
-			'awp-root': nodePath.resolve(srcScriptPath),
-			'awp-app': nodePath.resolve(srcScriptPath, 'features'),
-			'awp-home': nodePath.resolve(srcScriptPath, 'features/home'),
-			'awp-settings': nodePath.resolve(srcScriptPath, 'features/settings'),
-			'awp-model': nodePath.resolve(srcScriptPath, 'model'),
-			'awp-directives': nodePath.resolve(srcScriptPath, 'directives'),
-			'awp-services': nodePath.resolve(srcScriptPath, 'services'),
-		},
-		//extensions: [ '', '.html', '.js' ],
-	};
-
-	/**
 	 * Don't parse large libraries putting stuff at the global level.
 	 * http://stackoverflow.com/questions/28969861/managing-jquery-plugin-dependency-in-webpack
 	 */
@@ -137,11 +165,7 @@ function makeWebpackConfig()
 	 * Reference: http://webpack.github.io/docs/configuration.html#devtool
 	 * Type of sourcemap to use per build type.
 	 */
-	if (isTest)
-	{
-		config.devtool = 'inline-source-map';
-	}
-	else if (isDebug) // server mode (dev / debug)
+	if (isDebug) // server mode (dev / debug)
 	{
 		// config.devtool = 'eval-source-map'; // Perhaps faster to generate, but doesn't work on startup (eg. breakpoints in app.js)
 		// config.devtool = 'source-map'; // So switch to this one if you have to step in app.js
@@ -167,14 +191,6 @@ function makeWebpackConfig()
 		loaders:
 		[
 			{
-				// Load jQuery and expose it as global variable before loading AngularJS,
-				// so that the latter will use it instead of jqLite.
-				// Reference: https://github.com/webpack/expose-loader
-				// and http://stackoverflow.com/questions/36065931/webpack-how-to-make-angular-auto-detect-jquery-and-use-it-as-angular-element-in
-				test: require.resolve('jquery'), // Full path in node_modules
-				loader: 'expose?$!expose?jQuery'
-			},
-			{
 				// STYLUS LOADER
 				// Reference: https://github.com/shama/stylus-loader
 				// Allow processing Stylus files.
@@ -192,32 +208,19 @@ function makeWebpackConfig()
 				// Adds CSS to the DOM by injecting a <style> tag. Used in development.
 				test: /\.styl$/,
 				// loader: 'style-loader!css-loader!stylus-loader'
-				loader:
-					// Test: no CSS needed
-					isTest ?
-						'null' :
-						// Extract CSS to a separate file (loads in parallel with JS, so faster)
-						ExtractTextPlugin.extract('style', cssPipeline)
+				// Extract CSS to a separate file (loads in parallel with JS, so faster)
+				loader: ExtractTextPlugin.extract('style', cssPipeline)
 			},
 			{
 				// CSS LOADER
 				// For CSS files from modules.
 				test: /\.css$/,
-				loader:
-					isTest ?
-						'null' :
-						// Use the 'style' loader after treatment by the CSS loader (minification with source map)
-						ExtractTextPlugin.extract('style', 'css?sourceMap')
-			},
-			{
-				// HTML LOADER
-				// Reference: https://github.com/webpack/raw-loader
-				// Allow loading HTML through JS.
-				test: /\.html$/,
-				loader: 'raw'
+				// Use the 'style' loader after treatment by the CSS loader (minification with source map)
+				loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
 			},
 		],
 	};
+
 	// ASSET LOADER
 	// Reference: https://github.com/webpack/file-loader
 	// Copy asset files (images, fonts...) to output.
@@ -257,12 +260,12 @@ function makeWebpackConfig()
 				{
 					emitFile: false,
 					name: '[name].[ext]',
-					publicPath: isProd ? prodPath + 'images/' : srcAssetsPath + 'img/',
+					publicPath: prodPath + 'assets/img/',
 				}
 			}
 		);
 	}
-	else if (!isTest)
+	else
 	{
 		// Just copy the files as is
 		config.module.loaders.push(
@@ -293,109 +296,61 @@ function makeWebpackConfig()
 		})
 	];
 
-	// ISTANBUL LOADER
-	// Reference: https://github.com/deepsweet/istanbul-instrumenter-loader
-	// Instrument JS files with Istanbul for subsequent code coverage reporting.
-	// Skips node_modules and files that end with .test.js.
-	if (isTest && !isDebugTest)
-	{
-		config.module.preLoaders.push(
+	// Linters and hinters
+	config.module.preLoaders.push(
+		{
+			test: /\.html$/,
+			include: nodePath.resolve(srcPath + 'app/'),
+			loader: 'htmlhint'
+		},
+		{
+			test: /\.styl$/,
+			include: nodePath.resolve(srcPath + 'app/styles'),
+			loader: 'stylint'
+		},
 		{
 			test: /\.js$/,
 			include: nodePath.resolve(srcScriptPath),
-			exclude: /\.test\.js$/,
-			loader: 'istanbul-instrumenter'
-		});
-	}
-
-	if (!isTest)
+			loader: 'eslint-loader'
+		}
+	);
+	config.stylint =
 	{
-		config.module.preLoaders.push(
-			{
-				test: /\.html$/,
-				include: nodePath.resolve(srcPath + 'app/'),
-				loader: 'htmlhint'
-			},
-			{
-				test: /\.styl$/,
-				include: nodePath.resolve(srcPath + 'app/styles'),
-				loader: 'stylint'
-			},
-			{
-				test: /\.js$/,
-				include: nodePath.resolve(srcScriptPath),
-				loader: 'eslint-loader'
-			}
-		);
-		config.stylint =
-		{
-			config: nodePath.resolve(srcPath + '.stylintrc')
-		};
-	}
+		config: nodePath.resolve(srcPath + '.stylintrc')
+	};
 
-	/**
-	 * Plugins
-	 * Reference: http://webpack.github.io/docs/configuration.html#plugins
-	 * List: http://webpack.github.io/docs/list-of-plugins.html
-	 */
 	config.plugins =
 	[
-		// Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
-		// Dedupe modules in the output.
-		new webpack.optimize.DedupePlugin(),
+		// Reference: webpack-fail-plugin
+		// See also https://github.com/webpack/webpack/issues/708
+		WebpackFailPlugin,
 
-		// Reference: http://webpack.github.io/docs/list-of-plugins.html#ignoreplugin
-		// Ignore some modules. Here, avoids loading all locales of Moment.js (rather big! reduces vendor file of 165 KB).
-		// We can then require specific ones. See also http://stackoverflow.com/questions/25384360/how-to-prevent-moment-js-from-loading-locales-with-webpack
-//		new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-
-		// Reference: http://webpack.github.io/docs/list-of-plugins.html#provideplugin
-		// Automatically requires declared libraries creating global variables.
-		new webpack.ProvidePlugin(
+		// Reference: https://github.com/ampedandwired/html-webpack-plugin
+		// Render index.html, adding paths to generated style and JS files at appropriate places.
+		new HtmlWebpackPlugin(
 		{
-			$: 'jquery',
-			jQuery: 'jquery',
-			_: 'lodash',
-//			moment: 'moment',
-//			angular: 'angular', // No, we have to require it everywhere anyway
+			// Instrument the existing index
+			template: srcPath + 'index.html',
+			// Produces it there
+			filename: 'index.html',
+			// Put JS tags at the end of the body (not in the header)
+			inject: 'body'
 		}),
+
+		// Reference: https://github.com/webpack/extract-text-webpack-plugin
+		// Extract CSS to one file (otherwise it is inlined in the JS).
+		// Disabled when not in build mode.
+		new ExtractTextPlugin('[name].[chunkhash].css', { disable: !isProd }),
+
+		// Reference: https://github.com/webpack/docs/wiki/list-of-plugins#commonschunkplugin
+		// Separates the libraries from the application.
+		new webpack.optimize.CommonsChunkPlugin(
+		{
+			name: 'vendor',
+			filename: isProd ? 'vendor.[chunkhash].js' : 'vendor.bundle.js',
+			minChunks: Infinity
+		})
 	];
-
-	// Skip rendering index.html in test mode
-	if (!isTest)
-	{
-		config.plugins.push(
-			// Reference: webpack-fail-plugin
-			// See also https://github.com/webpack/webpack/issues/708
-			WebpackFailPlugin,
-
-			// Reference: https://github.com/ampedandwired/html-webpack-plugin
-			// Render index.html, adding paths to generated style and JS files at appropriate places.
-			new HtmlWebpackPlugin(
-			{
-				// Instrument the existing index
-				template: srcPath + 'index.html',
-				// Produces it there
-				filename: 'index.html',
-				// Put JS tags at the end of the body (not in the header)
-				inject: 'body'
-			}),
-
-			// Reference: https://github.com/webpack/extract-text-webpack-plugin
-			// Extract CSS to one file (otherwise it is inlined in the JS).
-			// Disabled when not in build mode.
-			new ExtractTextPlugin('[name].[chunkhash].css', { disable: !isProd }),
-
-			// Reference: https://github.com/webpack/docs/wiki/list-of-plugins#commonschunkplugin
-			// Separates the libraries from the application.
-			new webpack.optimize.CommonsChunkPlugin(
-			{
-				name: 'vendor',
-				filename: isProd ? 'vendor.[chunkhash].js' : 'vendor.bundle.js',
-				minChunks: Infinity
-			})
-		);
-	}
 
 	// Add build specific plugins
 	if (isProd)
@@ -418,12 +373,12 @@ function makeWebpackConfig()
 			new CopyWebpackPlugin(
 			[
 				{
-					from: srcAssetsPath + 'images',
-					to: prodPath + 'images'
+					from: srcAssetsPath + 'img',
+					to: 'assets/img/'
 				},
 				{
-					from: srcAssetsPath + 'images/favicon.ico',
-					to: prodPath
+					from: srcAssetsPath + 'img/favicon.ico',
+					to: './'
 				},
 			]),
 
@@ -452,3 +407,87 @@ function makeWebpackConfig()
 
 	return config;
 }
+
+function addResolve(config)
+{
+	/**
+	 * Resolve
+	 * Reference: http://webpack.github.io/docs/configuration.html#resolve
+	 * Alias some paths, to avoid using require('../../foo') depending on depth in hierarchy...
+	 */
+	config.resolve =
+	{
+		alias:
+		{
+			// awp for angular-webpack-playground
+			'awp-root': nodePath.resolve(srcScriptPath),
+			'awp-app': nodePath.resolve(srcScriptPath, 'features'),
+			'awp-home': nodePath.resolve(srcScriptPath, 'features/home'),
+			'awp-settings': nodePath.resolve(srcScriptPath, 'features/settings'),
+			'awp-model': nodePath.resolve(srcScriptPath, 'model'),
+			'awp-directives': nodePath.resolve(srcScriptPath, 'directives'),
+			'awp-services': nodePath.resolve(srcScriptPath, 'services'),
+		},
+		//extensions: [ '', '.html', '.js' ],
+	};
+
+	return config;
+}
+
+function addCommonLoaders(config)
+{
+	config.module.loaders.push(
+		{
+			// Load jQuery and expose it as global variable before loading AngularJS,
+			// so that the latter will use it instead of jqLite.
+			// Reference: https://github.com/webpack/expose-loader
+			// and http://stackoverflow.com/questions/36065931/webpack-how-to-make-angular-auto-detect-jquery-and-use-it-as-angular-element-in
+			test: require.resolve('jquery'), // Full path in node_modules
+			loader: 'expose?$!expose?jQuery'
+		},
+		{
+			// HTML LOADER
+			// Reference: https://github.com/webpack/raw-loader
+			// Allow loading HTML through JS.
+			test: /\.html$/,
+			loader: 'raw'
+		}
+	);
+
+	return config;
+}
+
+function addCommonPlugins(config)
+{
+	/**
+	 * Plugins
+	 * Reference: http://webpack.github.io/docs/configuration.html#plugins
+	 * List: http://webpack.github.io/docs/list-of-plugins.html
+	 */
+	config.plugins.push(
+		// Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
+		// Dedupe modules in the output.
+		new webpack.optimize.DedupePlugin(),
+
+		// Reference: http://webpack.github.io/docs/list-of-plugins.html#ignoreplugin
+		// Ignore some modules. Here, avoids loading all locales of Moment.js (rather big! reduces vendor file of 165 KB).
+		// We can then require specific ones. See also http://stackoverflow.com/questions/25384360/how-to-prevent-moment-js-from-loading-locales-with-webpack
+//		new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+
+		// Reference: http://webpack.github.io/docs/list-of-plugins.html#provideplugin
+		// Automatically requires declared libraries creating global variables.
+		new webpack.ProvidePlugin(
+		{
+			$: 'jquery',
+			jQuery: 'jquery',
+			_: 'lodash',
+//			moment: 'moment',
+//			angular: 'angular', // No, we have to require it everywhere anyway
+		})
+	);
+
+	return config;
+}
+
+
+module.exports = config;
